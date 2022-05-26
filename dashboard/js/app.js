af4af7b7b5308869d1140fe4b6392e511c99255f
obedondimu <ondimuobed@gmail.com>
2022-05-26 07:49:09 +0300

    Getting started with JavaScript

---
+++ dashboard/js/app.js
@@ -0,0 +1,24 @@
+const text = document.querySelector(".text");
+const changeButton = document.querySelector(".changeColor");
+const userList = document.querySelector(".item-list");
+const listInput = document.querySelector(".input-list")
+const addList = document.querySelector(".addListBtn");
+
+
+
+
+text.style.backgroundColor ="skyblue";
+changeButton.style.backgroundColor= "gray";
+
+changeButton.addEventListener("click", function(){
+    text.classList.toggle("change");
+    const newLi = document.createElement("LI");
+    const liContent = document.createTextNode(listInput.value);
+    
+    newLi.appendChild(liContent);
+    userList.appendChild(newLi);
+
+})
+
+
+
